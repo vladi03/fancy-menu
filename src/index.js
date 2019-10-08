18fbcc41cd206da94819c3b70b64fda2f814f90e
vmartinez <vladimir.martinez@bcdtravel.com>
2019-10-08 13:18:41 -0400

    create demo menu

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import './index.css'
 import React from 'react'
 import {render} from 'react-dom'
-import { SideStrip } from './menus/sideStrip/SideStrip';
-import { Home, Wallpaper, PieChart, Warning, Settings, Input,
-    Dashboard, DataUsage
-} from "@material-ui/icons";
-const mainLinks = [
-    {label : "Home", icon: Home, selected: true, link:"#home" },
-    {label : "Report", icon: Wallpaper, link:"#report" },
-    {label : "Admin", icon: PieChart,
-        subMenu: [
-            { label : "My Dashboard", icon: Dashboard, link:"#mydashboard" },
-            { label : "Data Discovery", icon: DataUsage, link:"#datausage" },
-        ]
-    }
-];
+import {DemoMenu} from "./menus";
 
-const bottomLinks = [
-    {label : "Messages", icon: Warning },
-    {label : "Settings", icon: Settings },
-    {label : "Logout", icon: Input, link:"#logout" }
-];
 
-render(<SideStrip mainLinks={mainLinks}
-                  bottomLinks={bottomLinks}
-                  expandMenu={false}
-                  userLabel="Jane Smith"
-                  imageUrl="https://material-ui.com/static/images/avatar/7.jpg"
-/>, document.querySelector('#app'));
+render(<DemoMenu />, document.querySelector('#app'));
+
---
+++ src/menus/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { SideStrip } from './sideStrip/SideStrip';
+
+import { Home, Wallpaper, PieChart, Warning, Settings, Input,
+    Dashboard, DataUsage
+} from "@material-ui/icons";
+const mainLinks = [
+    {label : "Home", icon: Home, selected: true, link:"#home" },
+    {label : "Report", icon: Wallpaper, link:"#report" },
+    {label : "Admin", icon: PieChart,
+        subMenu: [
+            { label : "My Dashboard", icon: Dashboard, link:"#mydashboard" },
+            { label : "Data Discovery", icon: DataUsage, link:"#datausage" },
+        ]
+    }
+];
+
+const bottomLinks = [
+    {label : "Messages", icon: Warning },
+    {label : "Settings", icon: Settings },
+    {label : "Logout", icon: Input, link:"#logout" }
+];
+
+export const DemoMenu = ()=> (
+    <SideStrip mainLinks={mainLinks}
+                 bottomLinks={bottomLinks}
+                 expandMenu={false}
+                 userLabel="Jane Smith"
+                 imageUrl="https://material-ui.com/static/images/avatar/7.jpg"
+/>);
